6428a608a5f0cb584ed7a8ff8dbb4b132e5ecdb3
Gabriel <grt.araujo@hotmail.com>
2021-03-15 14:40:54 -0300

    Fixing storage.js.

--- storage/storage.js
+++ storage/storage.js
@@ -7,9 +7,9 @@ class storage {
     store(product,quantity,price) {
         if(product!=undefined && typeof product == "string") {
             if(quantity!=undefined && typeof quantity == "number" &&
-            quantity >= 0)
+            quantity >= 0) {
                 content.quantity = quantity;
-                if(price!=undefined && typeof price == "number" && price > 0)
+                if(price!=undefined && typeof price == "number" && price > 0) {
                     try {
                         this.database.store(product,quantity,price);
                     } catch(error) {
@@ -17,14 +17,16 @@ class storage {
                         "database due to: "+error.name+": "+error.message,
                         "/storage/storage.js",1);
                     }
-                else
+                } else {
                     throw new TypeError(
                     "The price must be a non-negative number.",
                     "/storage/storage.js",1);
-            else
+                }
+            } else {
                 throw new TypeError(
                 "The quantity must be a positive number.",
                 "/storage/storage.js",1);
+            }
         } else {
             throw new TypeError("The product name must be a string.",
             "/storage/storage.js",1);
