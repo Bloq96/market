1403f4bdaec2a7b43ea03c0e9cba867ba8902558
Gabriel <grt.araujo@hotmail.com>
2021-03-15 17:00:46 -0300

    Updating storage.js.

--- storage/storage.js
+++ storage/storage.js
@@ -1,66 +1,113 @@
 class DatabaseError extends Error {};
-class DatabaseStoringError extends Error {};
-class DatabaseDropingError extends Error {};
-class DatabaseSearchingError extends Error {};
+class DatabaseStoringError extends DatabaseError {};
+class DatabaseDropingError extends DatabaseError {};
+class DatabaseSearchingError extends DatabaseError {};
+class DatabaseUpdatingError extends DatabaseError {};
 
 class storage {
     constructor(database) {
         this.database = database;
     }
-    drop(product) {
-        if(product!=undefined && typeof product == "string") {
-            try {
-                this.database.drop(product);
-            } catch(error) {
-                throw new DatabaseDropingError("Failed to drop data from the " +
-                "database due to: "+error.name+": "+error.message,
-                "/storage/storage.js");
-            }                 
-        } else {
-            throw new TypeError("The product name must be a string.",
-            "/storage/storage.js")
-        }
+}
+
+storage.drop = function(product) {
+    if(product!=undefined && typeof product == "string") {
+        try {
+            this.database.drop(product);
+        } catch(error) {
+            throw new DatabaseDropingError("Failed to drop data from the " +
+            "database due to: "+error.name+": "+error.message,
+            "/storage/storage.js");
+        }                 
+    } else {
+        throw new TypeError("The product name must be a string.",
+        "/storage/storage.js")
     }
-    search(reference) {
-        if(reference!=undefined && typeof reference == "string") {
-            try {
-                return this.database.search(reference);
-            } catch(error) {
-                throw new DatabaseSearchingError("Failed to search data in " +
-                "the database due to: "+error.name+": "+error.message,
-                "/storage/storage.js");
-            }
-        } else {
-            throw new TypeError("The reference must be a string.",
+}
+
+storage.search = function(reference) {
+    if(reference!=undefined && typeof reference == "string") {
+        try {
+            return this.database.search(reference);
+        } catch(error) {
+            throw new DatabaseSearchingError("Failed to search data in " +
+            "the database due to: "+error.name+": "+error.message,
             "/storage/storage.js");
         }
+    } else {
+        throw new TypeError("The reference must be a string.",
+        "/storage/storage.js");
     }
-    store(product,quantity,price) {
-        if(product!=undefined && typeof product == "string") {
-            if(quantity!=undefined && typeof quantity == "number" &&
-            quantity >= 0) {
-                if(price!=undefined && typeof price == "number" && price > 0) {
-                    try {
-                        this.database.store(product,quantity,price);
-                    } catch(error) {
-                        throw new DatabaseStoringError("Failed to store data " +
-                        "in the database due to: " + error.name + ": " +
-                        error.message, "/storage/storage.js");
-                    }
-                } else {
-                    throw new TypeError(
-                    "The price must be a non-negative number.",
-                    "/storage/storage.js");
+}
+
+storage.store = function(product,quantity,price) {
+    if(product!=undefined && typeof product == "string") {
+        if(quantity!=undefined && typeof quantity == "number" &&
+        quantity >= 0) {
+            if(price!=undefined && typeof price == "number" && price > 0) {
+                try {
+                    this.database.store(product,quantity,price);
+                } catch(error) {
+                    throw new DatabaseStoringError("Failed to store data " +
+                    "in the database due to: " + error.name + ": " +
+                    error.message, "/storage/storage.js");
                 }
             } else {
                 throw new TypeError(
-                "The quantity must be a positive number.",
+                "The price must be a non-negative number.",
                 "/storage/storage.js");
             }
         } else {
-            throw new TypeError("The product name must be a string.",
+            throw new TypeError(
+            "The quantity must be a positive number.",
             "/storage/storage.js");
         }
+    } else {
+        throw new TypeError("The product name must be a string.",
+        "/storage/storage.js");
+    }
+}
+
+storage.update = function(product,quantity,price) {
+    if(product!=undefined && typeof product == "string") {
+        if(quantity!=undefined && typeof quantity == "number" &&
+        quantity >= 0) {
+            if(price!=undefined && typeof price == "number" && price > 0) {
+                try {
+                    this.database.update(product,quantity,price);
+                } catch(error) {
+                    throw new DatabaseUpdatingError("Failed to update the " +
+                    "database data due to: " + error.name + ": " +
+                    error.message, "/storage/storage.js");
+                }
+            } else {
+                try {
+                    this.database.updateQuantity(product,quantity);
+                } catch(error) {
+                    throw new DatabaseUpdatingError("Failed to update the " +
+                    "quantity data due to: " + error.name + ": " +
+                    error.message, "/storage/storage.js");
+                }
+            }
+        } else {
+            if(price!=undefined && typeof price == "number" && price > 0) {
+                try {
+                    this.database.updatePrice(product,price);
+                } catch(error) {
+                    throw new DatabaseUpdatingError("Failed to update the " +
+                    "price data due to: " + error.name + ": " +
+                    error.message, "/storage/storage.js");
+                }
+            } else {
+                throw new TypeError(
+                "Cannot update product. Either the quantity must be a " +
+                "positive number or the price must be a non-negative number.",
+                "/storage/storage.js");
+            }
+        }
+    } else {
+        throw new TypeError("The product name must be a string.",
+        "/storage/storage.js");
     }
 }
 
@@ -68,4 +115,5 @@ module.exports = {"storage":storage,
                   "DatabaseError": DatabaseError,
                   "DatabaseDropingError": DatabaseDropingError,
                   "DatabaseSearchingError": DatabaseSearchingError,
-                  "DatabaseStoringError": DatabaseStoringError}
+                  "DatabaseStoringError": DatabaseStoringError,
+                  "DatabaseStoringError": DatabaseUpdatingError}
