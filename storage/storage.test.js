879a18f8d455e346ffd0dd86cd452391decbc776
Gabriel <grt.araujo@hotmail.com>
2021-03-15 13:52:07 -0300

    Improving the first test.

--- storage/storage.test.js
+++ storage/storage.test.js
@@ -3,6 +3,6 @@ const storage = require("./storage");
 test('checks if storage is calling the DB store method.', () => {
     let database = {"store": (product,quantity,price) =>
                                   [product, quantity, price]};
-    storage.setDatabase(database);
-    expect(storage.store("flash drives",18,8)).toBe(["flash drives", 18, 8]);
+    let mockStorage = new storage(database);
+    expect(mockStorage.store("flash drives",18,8)).toBe(["flash drives", 18, 8]);
 });
