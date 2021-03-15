e632c2f6cd7abff0bb69731f4b7f7e82e6f2b537
Gabriel <grt.araujo@hotmail.com>
2021-03-15 15:52:39 -0300

    Updating storage.js. All tests passing.

--- storage/storage.js
+++ storage/storage.js
@@ -1,8 +1,24 @@
 class DatabaseError extends Error {};
+class DatabaseStoringError extends Error {};
+class DatabaseDropingError extends Error {};
 
 class storage {
     constructor(database) {
         this.database = database;
+    }
+    drop(product) {
+        if(product!=undefined && typeof product == "string") {
+            try {
+                this.database.drop(product);
+            } catch(error) {
+                throw new DatabaseDropingError("Failed to drop data from the " +
+                "database due to: "+error.name+": "+error.message,
+                "/storage/storage.js");
+            }                 
+        } else {
+            throw new TypeError("The product name must be a string.",
+            "/storage/storage.js")
+        }
     }
     store(product,quantity,price) {
         if(product!=undefined && typeof product == "string") {
@@ -12,26 +28,28 @@ class storage {
                     try {
                         this.database.store(product,quantity,price);
                     } catch(error) {
-                        throw new DatabaseError("Failed to store data in the " +
-                        "database due to: "+error.name+": "+error.message,
-                        "/storage/storage.js",1);
+                        throw new DatabaseStoringError("Failed to store data " +
+                        "in the database due to: " + error.name + ": " +
+                        error.message, "/storage/storage.js");
                     }
                 } else {
                     throw new TypeError(
                     "The price must be a non-negative number.",
-                    "/storage/storage.js",1);
+                    "/storage/storage.js");
                 }
             } else {
                 throw new TypeError(
                 "The quantity must be a positive number.",
-                "/storage/storage.js",1);
+                "/storage/storage.js");
             }
         } else {
             throw new TypeError("The product name must be a string.",
-            "/storage/storage.js",1);
+            "/storage/storage.js");
         }
     }
 }
 
 module.exports = {"storage":storage,
-                  "DatabaseError": DatabaseError}
+                  "DatabaseError": DatabaseError,
+                  "DatabaseStoringError": DatabaseStoringError,
+                  "DatabaseDropingError": DatabaseDropingError}
