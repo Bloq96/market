9827ec24eb87fb4fd8190c8f463135271bc03a21
Betânia <grt.araujo@hotmail.com>
2021-03-22 15:06:19 -0300

    Adding sixth test. Changing in retun value test for storage search method. Two test failing.

--- sales/sales.test.js
+++ sales/sales.test.js
@@ -10,3 +10,24 @@ test("Checks whether the cart is being created properly.", () => {
     expect(testCart.getTotalCost()).toEqual(0);
     expect(testCart.getProducts()).toEqual([]);
 });
+
+test("Checks whether products are correctly added to the cart.", () => {
+    testCart.add({
+        "name": "Book",
+	"quantity": 1,
+	"price": 63
+    });
+    testCart.add({
+        "name": "Sewing Machine",
+	"quantity": 3,
+        "price": 59
+    });
+    testCart.add({
+        "name": "Book",
+	"quantity": 1,
+	"price": 48
+    });
+    expect(testCart.getProducts()).toEqual([
+    {"name": "Book", "quantity": 2, "price": 48},
+    {"name": "Sewing Machine", "quantity": 3, "price": 59}]);
+});
